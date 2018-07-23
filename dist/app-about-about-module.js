39ad9b861167f185edf6ef7b82269bb266939b12
kesavan <ykesan@gmail.com>
2018-07-23 06:48:55 +0530

    Initial commit.

---
+++ dist/app-about-about-module.js
@@ -0,0 +1,133 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app-about-about-module"],{
+
+/***/ "./src/app/about/about-routing.module.ts":
+/*!***********************************************!*\
+  !*** ./src/app/about/about-routing.module.ts ***!
+  \***********************************************/
+/*! exports provided: AboutRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutRoutingModule", function() { return AboutRoutingModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _about_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./about.component */ "./src/app/about/about.component.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    { path: '', component: _about_component__WEBPACK_IMPORTED_MODULE_2__["AboutComponent"] }
+];
+var AboutRoutingModule = /** @class */ (function () {
+    function AboutRoutingModule() {
+    }
+    AboutRoutingModule.components = [_about_component__WEBPACK_IMPORTED_MODULE_2__["AboutComponent"]];
+    AboutRoutingModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"])({
+            imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(routes)],
+            exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]]
+        })
+    ], AboutRoutingModule);
+    return AboutRoutingModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/about/about.component.html":
+/*!********************************************!*\
+  !*** ./src/app/about/about.component.html ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"view\">\n    <div class=\"container\">\n        <header>\n            <h3><span class=\"glyphicon glyphicon-info-sign\"></span> Development Team</h3>\n        </header>\n        <br />\n        <div class=\"container\">\n            <div class=\"row\">\n                <div class=\"col-md-2\">Achchuthan </div>\n                <div class=\"col-md-10\">PGIS/SC/MSC/CSC/17/01</div>\n            </div>\n            <br />\n            <div class=\"row\">\n                <div class=\"col-md-2\">Kesavan</div>\n                <div class=\"col-md-10\">PGIS/SC/MSC/CSC/17/12</div>\n            </div>\n            <br />\n            <div class=\"row\">\n                <div class=\"col-md-2\">Pavithra</div>\n                <div class=\"col-md-10\">PGIS/SC/MSC/CSC/17/13</div>\n            </div>\n            <br />\n            <div class=\"row\">\n                <div class=\"col-md-2\">Anuranga</div>\n                <div class=\"col-md-10\">PGIS/SC/MSC/CSC/17/21</div>\n            </div>\n            <br />\n            <div class=\"row\">\n                <div class=\"col-md-2\">Ethayaraj</div>\n                <div class=\"col-md-10\">PGIS/SC/MSC/CSC/17/25</div>\n            </div>\n        </div>\n    </div>\n</div>\n\n\n  \t\t\n  \t\t\n \t\t\n"
+
+/***/ }),
+
+/***/ "./src/app/about/about.component.ts":
+/*!******************************************!*\
+  !*** ./src/app/about/about.component.ts ***!
+  \******************************************/
+/*! exports provided: AboutComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutComponent", function() { return AboutComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AboutComponent = /** @class */ (function () {
+    function AboutComponent() {
+    }
+    AboutComponent.prototype.ngOnInit = function () {
+    };
+    AboutComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'cm-about',
+            template: __webpack_require__(/*! ./about.component.html */ "./src/app/about/about.component.html")
+        }),
+        __metadata("design:paramtypes", [])
+    ], AboutComponent);
+    return AboutComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/about/about.module.ts":
+/*!***************************************!*\
+  !*** ./src/app/about/about.module.ts ***!
+  \***************************************/
+/*! exports provided: AboutModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutModule", function() { return AboutModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _about_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about-routing.module */ "./src/app/about/about-routing.module.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+var AboutModule = /** @class */ (function () {
+    function AboutModule() {
+    }
+    AboutModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"])({
+            imports: [_about_routing_module__WEBPACK_IMPORTED_MODULE_1__["AboutRoutingModule"]],
+            declarations: [_about_routing_module__WEBPACK_IMPORTED_MODULE_1__["AboutRoutingModule"].components]
+        })
+    ], AboutModule);
+    return AboutModule;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=app-about-about-module.js.map
